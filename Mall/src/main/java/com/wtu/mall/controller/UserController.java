1c70b6a6c38d6b6299bc0a0be7363b541807bd31
XingXing <526225591@qq.com>
2022-06-14 10:33:55 +0800

    第一版

--- Mall/src/main/java/com/wtu/mall/controller/UserController.java
+++ Mall/src/main/java/com/wtu/mall/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.wtu.mall.controller;
 
+import com.baomidou.mybatisplus.core.MybatisSqlSessionFactoryBuilder;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.wtu.mall.Utils.UserMapperSqlSession;
 import com.wtu.mall.mapper.UserMapper;
 import com.wtu.mall.pojo.User;
 import com.wtu.mall.service.UserService;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 
@@ -20,10 +26,16 @@ public class UserController {
     @ResponseBody
     @RequestMapping("/")
     public User list() throws IOException {
-
-        SqlSession userSqlSession = UserMapperSqlSession.getUserSqlSession();
+        String resource = "mybatis--config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(inputStream);
+        SqlSession userSqlSession = sqlSessionFactory.openSession();
         UserMapper mapper = userSqlSession.getMapper(UserMapper.class);
-        List<User> list = mapper.getById();
+        User user = new User();
+        user.setName("ÕÅÈý");
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(user.getName() != null,User::getName,user.getName());
+        List<User> list = mapper.selectList(queryWrapper);
         return list.get(0);
     }
 }
--- Mall/src/main/java/com/wtu/mall/mapper/UserMapper.java
+++ Mall/src/main/java/com/wtu/mall/mapper/UserMapper.java
@@ -1,10 +1,11 @@
 package com.wtu.mall.mapper;
 
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wtu.mall.pojo.User;
 
 import java.util.List;
 
-public interface UserMapper  {
+public interface UserMapper extends BaseMapper<User> {
    List<User> getById();
 }
