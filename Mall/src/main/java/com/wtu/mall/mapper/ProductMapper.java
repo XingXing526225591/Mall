1f4dd5e0e737c8dae47c203f2427f0e3f4fd5df3
XingXing <526225591@qq.com>
2022-06-14 11:36:54 +0800

    mp版

---
+++ Mall/src/main/java/com/wtu/mall/mapper/ProductMapper.java
@@ -0,0 +1,7 @@
+package com.wtu.mall.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wtu.mall.pojo.Product;
+
+public interface ProductMapper extends BaseMapper<Product> {
+}
